85ff4aeb621f24985b79a1d01bdd457962bea494
Steve George <steve@pagerange.com>
2020-07-05 08:11:28 -0500

    Updated plugin

--- disable_gutenberg.php
+++ disable_gutenberg.php
@@ -13,8 +13,13 @@ License: MIT
 
 if (!defined('ABSPATH')) die('Direct script access is not allowed');
 
-add_filter("use_block_editor_for_post_type", "disable_gutenberg_editor");
-function disable_gutenberg_editor()
-{
-return false;
+if(!function_exists('disable_gutenberg_editor')) {
+
+    function disable_gutenberg_editor()
+    {
+        return false;
+    }
+
+    add_filter("use_block_editor_for_post_type", "disable_gutenberg_editor");
+
 }
